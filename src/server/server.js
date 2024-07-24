5c8765167c585fa5327a56be6904c025d092315d
nasywank1004 <pineapple.nanas4@gmail.com>
2024-07-24 18:50:31 +0700

    update

--- src/server/server.js
+++ src/server/server.js
@@ -8,8 +8,8 @@ const InputError = require('../exceptions/InputError');
 
 (async () => {
     const server = Hapi.server({
-        port: 3000,
-        host: 'localhost',
+        port: process.env.PORT || 8080,
+        host: '0.0.0.0',
         routes: {
             cors: {
                 origin: ['*'],
--- src/services/dataService.js
+++ src/services/dataService.js
@@ -1,13 +1,16 @@
 const { Firestore } = require('@google-cloud/firestore')
+const path = require('path')
+
+const keyFile = path.resolve('./key.json')
 
 async function storeData(id, data) {
-    const db = new Firestore({ projectId : 'submissionmlgc-nasywakamilia'})
+    const db = new Firestore({ projectId : 'submissionmlgc-nasywakamilia', keyFilename : keyFile})
     const predictCollection = db.collection('predictions')
     return predictCollection.doc(id).set(data)
 }
 
 async function getAllData() {
-    const db = new Firestore({ projectId : 'submissionmlgc-nasywakamilia'})
+    const db = new Firestore({ projectId : 'submissionmlgc-nasywakamilia',  keyFilename : keyFile})
     const predictionCollection = db.collection('predictions')
     const predictionHistories = await predictionCollection.get()
     return predictionHistories.docs
